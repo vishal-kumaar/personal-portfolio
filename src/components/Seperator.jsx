6327105ba9a8f20c10f788c529df787ed88837cb
Vishal Kumar <vy10607@gmail.com>
2023-02-21 16:46:20 +0530

    Created Seperator component

---
+++ src/components/Seperator.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import seperator from "../assets/images/seperator.svg";
+
+export default function Seperator(props) {
+  return (
+    <div
+      className={`${props.opacity} bg-gradient-to-r from-sky-500 to-indigo-500`}
+    >
+      <img src={seperator} alt="" className={`${props.rotate} w-full h-10`} />
+    </div>
+  );
+}
