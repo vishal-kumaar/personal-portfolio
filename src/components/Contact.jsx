6ed36989e7191a2161c474cca159ff63326347ba
Vishal Kumar <vy10607@gmail.com>
2023-02-23 17:31:47 +0530

    Add height in social media icon container

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -10,10 +10,18 @@ import file from "../assets/images/file.svg";
 
 export default function Contact(props) {
   return (
-    <div className={`${props.opacity} transition duration-500 ease-in-out bg-white py-14 flex flex-col md:flex-row justify-center items-center`}>
+    <div
+      className={`${props.opacity} transition duration-500 ease-in-out bg-white py-14 flex flex-col md:flex-row justify-center items-center`}
+    >
       <div className="mr-auto ml-auto md:mr-32 md:ml-0 w-fit">
-        <img src={user.profilePic} alt="" className="rounded-full w-52 mb-3" />
-        <h1 className="font-signika text-black text-xl">{user.name}</h1>
+        <div className="h-52">
+          <img
+            src={user.profilePic}
+            alt=""
+            className="rounded-full w-52 mb-3"
+          />
+        </div>
+        <h1 className="font-signika text-black text-xl ">{user.name}</h1>
         <p className="font-poppins text-md text-black mb-4">{user.gmail}</p>
         <a
           href={user.resumeLink}
@@ -35,7 +43,7 @@ export default function Contact(props) {
             rel="noreferrer"
             className="flex items-center w-28 my-2 mx-auto md:mx-0 transition ease-in-out duration-300 hover:scale-105"
           >
-            <div className="bg-blue-600 rounded-full p-2 shadow-xl shadow-black/30 outline-white hover:bg-blue-700 hover:shadow-black/80">
+            <div className="bg-blue-600 rounded-full p-2 shadow-xl shadow-black/30 outline-white hover:bg-blue-700 hover:shadow-black/80 h-10">
               <img src={linkedin} alt="linkedin" className="w-6 invert" />
             </div>
             <p className="ml-2 text-sm font-bold font-signika text-black/90">
@@ -50,7 +58,7 @@ export default function Contact(props) {
             rel="noreferrer"
             className="flex items-center w-28 my-2 mx-auto md:mx-0 transition ease-in-out duration-300 hover:scale-105"
           >
-            <div className="bg-black rounded-full p-2 shadow-xl shadow-black/30 outline-white hover:bg-black/80 ">
+            <div className="bg-black rounded-full p-2 shadow-xl shadow-black/30 outline-white hover:bg-black/80 h-10">
               <img src={github} alt="github" className="w-6 invert" />
             </div>
             <p className="ml-2 text-sm font-bold font-signika text-black/90">
@@ -65,7 +73,7 @@ export default function Contact(props) {
             rel="noreferrer"
             className="flex items-center w-28 my-2 mx-auto md:mx-0 transition ease-in-out duration-300 hover:scale-105"
           >
-            <div className="bg-blue-800 rounded-full p-2 shadow-xl shadow-black/30 outline-white hover:bg-blue-900 hover:shadow-black/80">
+            <div className="bg-blue-800 rounded-full p-2 shadow-xl shadow-black/30 outline-white hover:bg-blue-900 hover:shadow-black/80 h-10">
               <img src={facebook} alt="facebook" className="w-6 invert" />
             </div>
             <p className="ml-2 text-sm font-bold font-signika text-black/90">
@@ -80,7 +88,7 @@ export default function Contact(props) {
             rel="noreferrer"
             className="flex items-center w-28 my-2 mx-auto md:mx-0 transition ease-in-out duration-300 hover:scale-105"
           >
-            <div className="bg-red-700 rounded-full p-2 shadow-xl shadow-black/30 outline-white hover:bg-red-800 hover:shadow-black/80">
+            <div className="bg-red-700 rounded-full p-2 shadow-xl shadow-black/30 outline-white hover:bg-red-800 hover:shadow-black/80 h-10">
               <img src={instagram} alt="facebook" className="w-6 invert" />
             </div>
             <p className="ml-2 text-sm font-bold font-signika text-black/90">
@@ -95,7 +103,7 @@ export default function Contact(props) {
             rel="noreferrer"
             className="flex items-center w-28 my-2 mx-auto md:mx-0 transition ease-in-out duration-300 hover:scale-105"
           >
-            <div className="bg-blue-500 rounded-full p-2 shadow-xl shadow-black/30 outline-white hover:bg-blue-600 hover:shadow-black/80">
+            <div className="bg-blue-500 rounded-full p-2 shadow-xl shadow-black/30 outline-white hover:bg-blue-600 hover:shadow-black/80 h-10">
               <img src={twitter} alt="twitter" className="w-6 invert" />
             </div>
             <p className="ml-2 text-sm font-bold font-signika text-black/90">
@@ -110,7 +118,7 @@ export default function Contact(props) {
             rel="noreferrer"
             className="flex items-center w-28 my-2 mx-auto md:mx-0 transition ease-in-out duration-300 hover:scale-105"
           >
-            <div className="bg-blue-600 rounded-full p-2 shadow-xl shadow-black/30 outline-white hover:bg-blue-700 hover:shadow-black/80">
+            <div className="bg-blue-600 rounded-full p-2 shadow-xl shadow-black/30 outline-white hover:bg-blue-700 hover:shadow-black/80 h-10">
               <img src={hashnode} alt="twitter" className="w-6" />
             </div>
             <p className="ml-2 text-sm font-bold font-signika text-black/90">
