00ce0ed5b758aa7bc88313c718bb1c9e5fb7f233
Vishal Kumar <vy10607@gmail.com>
2023-02-25 16:32:04 +0530

    Changed window.onclick to window.addEventListener

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -9,16 +9,18 @@ export default function Project(props) {
     document.body.style.overflow = "auto";
   };
 
-  window.onclick = (event) => {
+  window.addEventListener("click", (event) => {
     if (event.target.id === "project") {
       closeModal();
     }
-  };
+  });
 
   return (
     <div
       id="project"
-      className={`bg-transparent ${props.modal ? "translate-y-0" : "-translate-y-full"} overflow-auto w-full h-full fixed top-0 left-0 z-50 transition duration-700 ease-in-out flex`}
+      className={`bg-transparent ${
+        props.modal ? "translate-y-0" : "-translate-y-full"
+      } overflow-auto w-full h-full fixed top-0 left-0 z-50 transition duration-700 ease-in-out flex`}
     >
       {props.project && (
         <div className="bg-white h-fit relative w-[95%] m-auto px-8 py-14 flex flex-col lg:flex-row justify-center items-center rounded-xl">
@@ -34,7 +36,9 @@ export default function Project(props) {
             className="w-96 mb-5 lg:mb-0 rounded-xl border-2"
           />
           <div className="ml-0 lg:ml-10 max-w-3xl w-full">
-            <h3 className="font-firasans text-black text-sm mb-1">{props.project.tech}</h3>
+            <h3 className="font-firasans text-black text-sm mb-1">
+              {props.project.tech}
+            </h3>
             <h1 className="font-signika text-black text-2xl lg:text-3xl mb-1">
               {props.project.name}
             </h1>
